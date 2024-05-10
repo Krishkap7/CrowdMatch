71d5e39f704ef4a39bfbfab232d8194dc2062c4c
Krishkap7 <krishkap7@gmail.com>
2024-05-10 01:08:19 -0700

    neovim test

---
+++ test.py
@@ -0,0 +1,8 @@
+# foo is a test function that I am writing to display a certain amount of capability in neovim
+def foo(int start):
+    if (start = 0):
+        return True;
+    return False;
+    
+if __name__ == "__main__":
+    main()
